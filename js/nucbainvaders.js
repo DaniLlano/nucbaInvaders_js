7d423ef09d35ac9cb78d4e8f89901be1b8efc3b0
Dani <danillano97@gmail.com>
2021-11-10 23:33:33 -0300

    nucbainvaders

---
+++ js/nucbainvaders.js
@@ -0,0 +1,42 @@
+/*
+The game class
+
+This class represents a Space Invaders game.
+Create an insance of it, change any of the default values in the settings and call 'start' to run the game.
+
+Call 'initialise' before 'start' to set canvas the game will draw to.
+
+Call 'moveShip' or 'shipFare' to control the ship.
+
+Listen for 'gameWon' or 'gameLost' event to handle the game ending.
+*/
+
+// constants for the keyboard
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+var KEY_SPACE = 32;
+
+// create an instance of the game class
+function Game() {
+    // set the initial config
+    this.config = {
+        bombRate: 0.05,
+        bombMinVelocity: 50,
+        bombMaxVelocity: 50,
+        invaderInitialVelocity: 25,
+        invaderAcceleration: 0,
+        invaderDropDistance: 20,
+        rocketVelocity: 120,
+        rocketMaxFire: 2,
+        gameWidth: 400,
+        gameHeight: 300,
+        fps: 50,
+        debugMode: false,
+        invaderRanks: 5,
+        invaderFiles: 10,
+        shipSpeed: 120,
+        levelDifficultyMultiplier: 0.2,
+        pointsPerInvader: 5,
+        limitLevelIncrease: 25
+    }
+}
